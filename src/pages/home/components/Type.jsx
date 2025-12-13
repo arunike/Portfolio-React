dd50fa4d24116fd31d1b98145f09c1c2926a567e
Richie Zhou <r631137986@gmail.com>
2025-12-12 22:10:45 -0800

    init

---
+++ src/pages/home/components/Type.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "A Software Engineer",
+          "A Full-Stack Developer",
+          "A Front-End Developer",
+          "A Back-End Developer",
+          "A Mobile Developer",
+          "A Software Engineer Intern",
+          "A Software Developer Intern",
+          "A Full-Stack Developer Intern",
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
